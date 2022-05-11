fb33eae5ccfd89327f4ad61ebecd6ff5c5c962c0
Parkhw317 <sukisimmax@gmail.com>
2022-05-11 18:20:39 +0900

    [이찬희, 박희원] BasketFilter 생성 > 장바구니에서 구매버튼 눌렀을때 로그인 상태가 아니면 로그인 페이지로 이동 / ReviewFilter 생성 > 상품 리뷰 작성 때 로그인 상태가 아니면 로그인 페이지로 이동

---
+++ dogFoodShoppingMall/src/main/java/filter/ReviewFilter.java
@@ -0,0 +1,34 @@
+package filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebFilter("/insertReviewController")
+public class ReviewFilter implements Filter {
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+	     if(request instanceof HttpServletRequest) { // request가 HttpServletRequest로 변경가능하다면 
+			HttpServletRequest req = (HttpServletRequest)request; 
+			HttpSession session = req.getSession(); //세션 불러오기 
+			String memberId = (String)session.getAttribute("sessionMemberId"); 
+			
+			if(memberId == null) {
+				if(response instanceof HttpServletResponse) {
+					((HttpServletResponse)response).sendRedirect(req.getContextPath()+"/loginController");
+				} 
+			}
+	     }
+	     chain.doFilter(request, response);
+	}
+
+}
