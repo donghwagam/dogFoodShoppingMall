fd6f4b20527e2616ecc690ff33f92962ee32dd9d
ohi22 <ohi2262@naver.com>
2022-05-17 11:31:20 +0900

    [오혜인, 감동화] deleteDogController 생성 > 강아지 정보 삭제 구현 / + 그 전 커밋메시지 : issue(2개 이상 넘길때 회원 장바구니에서 +1만 되는 문제) 발생 해결 / BasketListController 수정 > select 부분 input으로 변경 / DogDao 수정 > deleteDog() 추가

---
+++ dogFoodShoppingMall/src/main/java/controller/deleteDogController.java
@@ -0,0 +1,34 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.DogDao;
+
+@WebServlet("/deleteDogController")
+public class deleteDogController extends HttpServlet {
+	private DogDao dogDao;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.dogDao = new DogDao();
+		
+		//기능 테스트 위한 임의의 값 넣음
+		int memberDogId = 2;
+		
+		//삭제 구현 위한 메서드 호출해서 강아지 정보 삭제
+		dogDao.deleteDog(memberDogId);
+		
+		//삭제 했으면 다시 회원 정보 보여주는 컨트롤러로 리다이렉트
+		response.sendRedirect(request.getContextPath()+"/memberOneController");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
